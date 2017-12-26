b2ce517380e3664d725d9493bedd61c242bfeb49
kumataro <r116309@g.kobe-kosen.ac.jp>
2017-12-26 18:26:51 +0900

    ウィンドウの表示

---
+++ Game Launcher/wndproc.h
@@ -0,0 +1,7 @@
+#pragma once
+
+
+#include <Windows.h>
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp);
---
+++ Text Editor/Comment Editor/wndproc.cpp
@@ -0,0 +1,7 @@
+#include "wndproc.h"
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+
+}
