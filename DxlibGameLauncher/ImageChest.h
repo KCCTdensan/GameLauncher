a12c28cd233334fa2e6822b9d121b4229227d2a7
irimoti <irimoto.s@gmail.com>
2019-09-21 19:39:18 +0900

    chest追加完了

--- DxlibGameLauncher/ImageChest.h
+++ DxlibGameLauncher/ImageChest.h
@@ -1,5 +1,22 @@
 #pragma once
-class ImageChest
+
+#include <DxLib.h>
+#include "OperationData.h"
+#include <unordered_map>
+
+
+enum ImageHandleNo
 {
+	a,//add handle name
+	NumImageHandle
 };
 
+class ImageChest
+{
+	int chest[NumImageHandle];
+
+public:
+	ImageChest();
+	~ImageChest();
+	int GetImageHandle(ImageHandleNo handleName);
+};
