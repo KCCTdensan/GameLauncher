c79113dc7c5cc2f250b44bd20e8abad8dc3d2059
irimoti <irimoto.s@gmail.com>
2019-09-22 19:24:11 +0900

    コメント消去

--- DxlibGameLauncher/ObjectManager.cpp
+++ DxlibGameLauncher/ObjectManager.cpp
@@ -3,65 +3,6 @@
 #include "InputManager.h"
 
 
-/*
-
-
-		int ObjectType;
-
-	WCHAR name;
-
-	bool ExistenceFlag = FALSE;//存在フラグ
-	bool EffectiveFlag = TRUE;//有効フラグ
-
-	bool ActivationFlag;
-	bool MouseFlag;
-
-	int AnimationBlend;
-	bool AnimationFlag;
-	int AnimationTick = 0;
-
-	bool RoundnessFlag;
-	int RoundnessSize;
-
-	int x;
-	int y;
-	int xSize;
-	int ySize;
-
-	bool insideFlag;
-	bool outsideFlag;
-	int insideColor;
-	int outsideColor;
-	int outsidePixel;
-
-	bool pictureFlag;
-	int pictureEnum;
-
-	bool WritingFlag = FALSE;
-	WCHAR writing;
-	int writingX;
-	int writingY;
-	int WritingWidth;
-
-	int writingColor;
-	int writingFont;
-	int writingSize;
-
-	int FontHandle;
-
-	int WritingArrengement;
-
-	bool ListFlag;
-	WCHAR ListData[LIST_MAX];
-	int ListDataX[LIST_MAX];
-	int ListDataY[LIST_MAX];
-	int ListDataWidth[LIST_MAX];
-	int ListOneSizeheight[LIST_MAX];
-
-	変数一覧です。現在のobjectdata一つずつに入ってるものです
-
-*/
-
 ObjectManager::ObjectManager()
 {
 }
