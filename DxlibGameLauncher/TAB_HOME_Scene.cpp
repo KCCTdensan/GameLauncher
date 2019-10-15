70c7173f13ccc57e038e265640f089f0bf137544
irimoti <irimoto.s@gmail.com>
2019-10-16 00:13:22 +0900

    json

--- DxlibGameLauncher/TAB_HOME_Scene.cpp
+++ DxlibGameLauncher/TAB_HOME_Scene.cpp
@@ -6,4 +6,6 @@ TAB_HOME_Scene::TAB_HOME_Scene(ObjectManager &objectManager)
 {
 	objectManager.Set(L"test", 100, 100, 100, 100, BUTTON);
 	objectManager.ColorSet(L"test", FALSE, NULL, NULL, TRUE, GetColor(255, 255, 255));
+	objectManager.WritingSet(L"test", TRUE, L"アイスクリーム");
+	objectManager.WritingFontSet(L"test", 10, GetColor(255, 0, 255), ARRANGEMENT_X_LEFT, ARRANGEMENT_Y_CENTER);
 }
