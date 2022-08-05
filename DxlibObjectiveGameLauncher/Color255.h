060e467ba283d0d4e70865b7e96af17192ec28de
isirmt <r119306@g.kobe-kosen.ac.jp>
2022-08-05 10:59:39 +0900

    ColorStruxt

---
+++ DxlibObjectiveGameLauncher/Color255.h
@@ -0,0 +1,16 @@
+#pragma once
+
+struct Color255 {
+	int r;
+	int g;
+	int b;
+	int a;
+	Color255(int _grayScale)
+		:r(_grayScale), g(_grayScale), b(_grayScale), a(255) {}
+	Color255(int _grayScale, int _a)
+		:r(_grayScale), g(_grayScale), b(_grayScale), a(_a) {}
+	Color255(int _r, int _g, int _b)
+		:r(_r), g(_g), b(_b), a(255) {}
+	Color255(int _r, int _g, int _b, int _a)
+		:r(_r), g(_g), b(_b), a(_a) {}
+};
